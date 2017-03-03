3ac8dbf42b149f410d5cfb04a1dd71c5efd4a0ed
huipo22 <huipo22@163.com>
2017-03-03 13:39:14 +0800

    net 模块

--- chat.js
+++ chat.js
@@ -1,3 +1,25 @@
 /**
  * Created by huipo on 17-3-3.
  */
+var net = require('net'); //引入net模块
+var ChatServer = net.createServer();//net.createServer()方法  建立一个服务器
+
+clientList = [];
+// 服务端监听 'connection' 连接 方法
+ChatServer.on('connection', function (client) { //client 就是客户端
+    clientList.push(client);
+    client.write('Welcome to huipo chat server ....  \n'); //client.write()  向客户端输出
+    // client.write('This server building...  \n');
+    // client.end();//end()关闭客户端连接
+
+    //客户端监听 'data' 方法
+    client.on('data', function (data) {
+        // console.log(data.toString());
+        for (var i = 0; i < clientList.length; i++) {
+            if (client != clientList[i]) {
+                clientList[i].write(data)
+            }
+        }
+    })
+})
+ChatServer.listen(9000);
