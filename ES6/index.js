abea34529547170a6ecfbf271210d5c28d864d05
hu <huipo22@163.com>
2017-06-28 16:30:03 +0800

    setTimeout

--- ES6/index.js
+++ ES6/index.js
@@ -21,3 +21,51 @@
  * 数值扩展
  * */
 
+// json 数据格式
+// let json = {
+//     '0':'json1',
+//     '1':'json2',
+//     '2':'json3',
+//     lenght:3
+// }
+// let arr=Array.form(json);
+// console.log(arr);
+
+// for(var i=0;i<5;i++){
+//     (function(j){
+//         setTimeout(function(){
+//             console.log(new Date,j)
+//         },1000)
+//     })(i)
+// }
+// console.log(new Date,i)
+// for(var i=0;i<5;i++){
+//     setTimeout(function(j) {
+//         console.log(new Date,j)
+//     }, 1000,i);
+// }
+// console.log(new Date,i)
+// function output(j){
+//     setTimeout(function(){
+//         console.log(new Date,j)
+//     },1000)
+// }
+// for(var i=0;i<5;i++){
+//     output(i)
+// }
+// console.log(new Date,i)
+const tasks=[];
+const output = (i) =>new Promise((resolve)=>{
+    setTimeout(function() {
+        console.log(new Date,i);
+        resolve()
+    }, 1000*i);
+})
+for(var i=0;i<5;i++){
+    tasks.push(output(i))
+}
+Promise.all(tasks).then(()=>{
+    setTimeout(()=>{
+        console.log(new Date,i)
+    },1000)
+})
